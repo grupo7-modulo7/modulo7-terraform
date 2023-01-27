e6b265b60f342e9d994e36c41aef2dc67439984d
danmoralesletelier <115748177+danmoralesletelier@users.noreply.github.com>
2023-01-27 14:19:58 -0300

    Update repos.sh

--- dmorales/parte1/Ansible/repos.sh
+++ dmorales/parte1/Ansible/repos.sh
@@ -1,4 +1,4 @@
 cd /tmp
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo --no-check-certificate
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
